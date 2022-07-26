9313b4d0b96dc37755b7248897043650be19d7b3
aleksandar-koprivica-nimbus-tech <aleksandar.koprivica@nimbus-tech.io>
2022-07-26 13:48:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# NimbusTech ESLINT CONFIG
+
+
+
+## How to use
+
+`yarn add eslint-config-nimbus-tech`
+
+Add to `extends` in `eslintrc.js` as 
+
+`extends: [
+    'nimbus-tech',
+]`
+
+## How to add new rules?
+
+Open pull request with new updates and wait for new version release
+
+For more info, contact [NimbusTech](https://nimbus-tech.io/).
---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports = {
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        'import/order': [
+            'error',
+            {
+                alphabetize: { order: 'asc' },
+                'newlines-between': 'always',
+                pathGroupsExcludedImportTypes: ['builtin'],
+            },
+        ],
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: 'default',
+                format: ['camelCase'],
+                leadingUnderscore: 'allow',
+                trailingUnderscore: 'allow',
+            },
+            {
+                selector: 'function',
+                format: ['PascalCase'],
+                leadingUnderscore: 'allow',
+                trailingUnderscore: 'allow',
+            },
+            {
+                selector: 'variable',
+                format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+                leadingUnderscore: 'allow',
+                trailingUnderscore: 'allow',
+            },
+            {
+                selector: 'typeLike',
+                format: ['PascalCase'],
+            },
+            {
+                selector: 'method',
+                modifiers: ['static'],
+                format: ['UPPER_CASE'],
+            },
+            {
+                selector: 'enumMember',
+                format: ['UPPER_CASE'],
+            },
+            {
+                selector: 'enum',
+                format: ['UPPER_CASE'],
+            },
+            {
+                selector: 'property',
+                format: null,
+            },
+        ],
+    },
+}
