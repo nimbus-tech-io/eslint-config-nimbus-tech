a46295d8b6790baddcf73a1b0ab2bfc109130b70
Milos <milos.sikic@nimbus-tech.io>
2023-02-28 11:46:37 +0100

    further refine the styleguide

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-# NimbusTech eslint Config
+# NimbusTech eslint config
 
-## How to use
+Welcome to the config! All NimbusTech projects should use this eslint config to ensure a consistent coding style.
+
+## How to use it
+
+Install it via:
 
 `yarn add eslint-config-nimbus-tech`
 
-Add to `extends` in `eslintrc.js` as
+And then add it to the `extends` array in your `eslintrc.js`:
 
-```
+```js
 extends: [
   'nimbus-tech',
 ]
 ```
 
-**Be sure to install all peer dependencies!**
-
 ## How to add new rules?
 
-Open pull request with new updates and wait for new version release
+Open a pull request with new updates and wait for a new version to be released.
 
 For more info, contact [NimbusTech](https://nimbus-tech.io/).
--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 module.exports = {
-    rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-        'import/order': [
-            'error',
-            {
-                alphabetize: { order: 'asc' },
-                'newlines-between': 'always',
-                pathGroupsExcludedImportTypes: ['builtin'],
-            },
-        ],
-        'unused-imports/no-unused-imports': 'error',
-        '@typescript-eslint/naming-convention': [
-            'error',
-            {
-                selector: 'default',
-                format: ['camelCase'],
-                leadingUnderscore: 'allow',
-                trailingUnderscore: 'allow',
-            },
-            {
-                selector: 'function',
-                format: ['camelCase'],
-                leadingUnderscore: 'allow',
-                trailingUnderscore: 'allow',
-            },
-            {
-                selector: 'variable',
-                format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
-                leadingUnderscore: 'allow',
-                trailingUnderscore: 'allow',
-            },
-            {
-                selector: 'typeLike',
-                format: ['PascalCase'],
-            },
-            {
-                selector: 'method',
-                modifiers: ['static'],
-                format: ['UPPER_CASE'],
-            },
-            {
-                selector: 'enumMember',
-                format: ['UPPER_CASE'],
-            },
-            {
-                selector: 'enum',
-                format: ['UPPER_CASE'],
-            },
-            {
-                selector: 'property',
-                format: null,
-            },
-        ],
-    },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc' },
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['strictCamelCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'function',
+        format: ['strictCamelCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'variable',
+        format: ['strictCamelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'method',
+        modifiers: ['static'],
+        format: ['strictCamelCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+      },
+      {
+        selector: 'enum',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'property',
+        format: null, // TODO: Ask
+      },
+    ],
+  },
 };
