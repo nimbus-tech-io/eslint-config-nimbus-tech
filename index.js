f0d982b80ab06aaa136f630a69387cc2fbafc9ad
aleksandra-mitro-nimbus-tech <117164754+aleksandra-mitro-nimbus-tech@users.noreply.github.com>
2023-03-13 16:16:52 +0100

    feat: flat configuration

--- index.js
+++ index.js
@@ -1,82 +1,106 @@
-module.exports = {
-  plugins: ['import', 'unused-imports', 'check-file'],
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/ban-types': 'off',
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+import pluginImport from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
+import checkFile from "eslint-plugin-check-file";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import tsParser from '@typescript-eslint/parser';
 
-    'import/order': [
-      'error',
-      {
-        alphabetize: { order: 'asc' },
-        'newlines-between': 'always',
-        pathGroupsExcludedImportTypes: ['builtin'],
-      },
-    ],
+export default [
+  {
+    plugins: {
+      "import": pluginImport,
+      "unused-imports": unusedImports,
+      "check-file": checkFile,
+      "@typescript-eslint": typescriptEslint,
+      "prettier": prettier,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { modules: true },
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+      }
+    },
+    files: ['app/**/*.ts', 'app/**/*.tsx'],
+    rules: {
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/ban-types': 'off',
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
 
-    'unused-imports/no-unused-imports': 'error',
-
-    '@typescript-eslint/naming-convention': [
-      'error',
-      {
-        selector: 'default',
-        format: ['strictCamelCase'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'forbid',
-      },
-      {
-        selector: 'function',
-        format: ['strictCamelCase'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'forbid',
-      },
-      {
-        selector: 'variable',
-        format: ['strictCamelCase', 'StrictPascalCase', 'UPPER_CASE'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'forbid',
-      },
-      {
-        selector: 'typeLike',
-        format: ['StrictPascalCase'],
-      },
-      {
-        selector: 'method',
-        modifiers: ['static'],
-        format: ['strictCamelCase'],
-      },
-      {
-        selector: 'enumMember',
-        format: ['UPPER_CASE'],
-      },
-      {
-        selector: 'enum',
-        format: ['StrictPascalCase'],
-      },
-      {
-        selector: 'interface',
-        format: ['StrictPascalCase'],
-        custom: {
-          regex: '^I[A-Z]',
-          match: false,
+      'import/order': [
+        'error',
+        {
+          alphabetize: {order: 'asc'},
+          'newlines-between': 'always',
+          pathGroupsExcludedImportTypes: ['builtin'],
         },
-      },
-      {
-        selector: 'objectLiteralProperty',
-        format: ['UPPER_CASE', 'strictCamelCase'],
-      },
-    ],
+      ],
 
-    'check-file/filename-naming-convention': [
-      'error',
-      {
-        '**/*.{js,ts}': 'KEBAB_CASE',
-      },
-      {
-        ignoreMiddleExtensions: true,
-      },
-    ],
-    'check-file/folder-naming-convention': ['error', { 'src/**/': 'KEBAB_CASE' }],
-  },
-};
+      'unused-imports/no-unused-imports': 'error',
+
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['strictCamelCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'forbid',
+        },
+        {
+          selector: 'function',
+          format: ['strictCamelCase'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'forbid',
+        },
+        {
+          selector: 'variable',
+          format: ['strictCamelCase', 'StrictPascalCase', 'UPPER_CASE'],
+          leadingUnderscore: 'allow',
+          trailingUnderscore: 'forbid',
+        },
+        {
+          selector: 'typeLike',
+          format: ['StrictPascalCase'],
+        },
+        {
+          selector: 'method',
+          modifiers: ['static'],
+          format: ['strictCamelCase'],
+        },
+        {
+          selector: 'enumMember',
+          format: ['UPPER_CASE'],
+        },
+        {
+          selector: 'enum',
+          format: ['StrictPascalCase'],
+        },
+        {
+          selector: 'interface',
+          format: ['StrictPascalCase'],
+          custom: {
+            regex: '^I[A-Z]',
+            match: false,
+          },
+        },
+        {
+          selector: 'objectLiteralProperty',
+          format: ['UPPER_CASE', 'strictCamelCase'],
+        },
+      ],
+
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          '**/*.{js,ts}': 'KEBAB_CASE',
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      'check-file/folder-naming-convention': ['error', {'app/**/': 'KEBAB_CASE'}],
+    }
+  }
+];
